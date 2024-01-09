3bfcb5db497f48d9324f311514964802b2428e45
joelwright-atio <89510337+joelwright-atio@users.noreply.github.com>
2024-01-09 17:49:38 +0100

    initial commit

---
+++ src/test/java/de/magicobject/SpringBootExample/SpringBootExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package de.magicobject.SpringBootExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
